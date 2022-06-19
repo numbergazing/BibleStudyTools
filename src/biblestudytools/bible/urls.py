998468196f1a520f98e5532517267fe41b10775e
numbergazing <manoahbernier@gmail.com>
2022-06-19 09:25:38 +0200

    Updating construct_chapter_url with check_bible_version_and_book_slug usage and adding a new function construct_book_url to get book url

--- src/biblestudytools/bible/urls.py
+++ src/biblestudytools/bible/urls.py
@@ -1,9 +1,4 @@
-__all__ = ["construct_chapter_url"]
-
-from sys import exit
-
-from .util import bible_versions, bible_version_codes, version_books
-from .exceptions import BibleVersionDoesNotExistError, BookDoesNotExistError
+from .exceptions import check_bible_version_and_book_slug
 
 _home = "https://www.biblestudytools.com"
 _bible = _home + "/{version_code}"
@@ -11,24 +6,19 @@ _book = _bible + "/{book_slug}"
 _chapter = _book + "/{chapter_num}.html"
 
 
+def construct_book_url(bible_version: str, book_slug: str) -> str:
+
+    bible_version_code: str
+
+    bible_version_code = check_bible_version_and_book_slug(bible_version, book_slug)
+    return _book.format(version_code=bible_version_code, book_slug=book_slug)
+
+
 def construct_chapter_url(bible_version: str, book_slug: str, chapter_num: int) -> str:
 
     bible_version_code: str
 
-    if bible_version not in bible_versions.keys():
-        raise BibleVersionDoesNotExistError
+    bible_version_code = check_bible_version_and_book_slug(bible_version, book_slug)
+    # TODO Check if given chapter num isn't bigger than the available number of chapters
 
-    try:
-        bible_version_code = bible_version_codes[bible_version]
-        this_version_books = version_books[bible_version]
-
-        if book_slug not in this_version_books:
-            raise BookDoesNotExistError
-
-        # TODO Check if given chapter num isn't bigger than the available number of chapters
-
-    except ValueError:
-        print("An existing Bible version has no code or assigned books.")
-        exit(1)
-    else:
-        return _chapter.format(version_code=bible_version_code, book_slug=book_slug, chapter_num=chapter_num)
+    return _chapter.format(version_code=bible_version_code, book_slug=book_slug, chapter_num=chapter_num)
