79a57e07da77062d70c8b98982030cf1cf8f6a59
numbergazing <manoahbernier@gmail.com>
2022-06-21 08:29:58 +0200

    Adding get_verse function to get a single verse

--- src/biblestudytools/bible/nav.py
+++ src/biblestudytools/bible/nav.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup, ResultSet
 
 from biblestudytools.bible.urls import construct_chapter_url
-from biblestudytools.bible.util import bible_versions, version_books, chain_replace
+from biblestudytools.bible.util import bible_versions, version_books, chain_replace, Verse
 from biblestudytools.nav import get_markup
 
 
@@ -27,3 +27,22 @@ def get_verses(bible_version: str, book_slug: str, chapter_num: int) -> list[dic
         }
         for div in filtered_divs
     ]
+
+
+def get_verse(bible_version: str, book_slug: str, chapter_num: int, verse_num: int) -> Verse:
+
+    url: str
+    html: BeautifulSoup
+    filtered_divs: list
+
+    url = construct_chapter_url(bible_version, book_slug, chapter_num)
+    html = get_markup(url)
+    div = html.find("div", attrs={"data-verse-id": verse_num})
+
+    return Verse(
+        bible_versions[bible_version],
+        version_books[bible_version][book_slug],
+        chapter_num,
+        verse_num,
+        chain_replace(div.text, ["\n", f"{int(div.a.text)}"], "").strip(),
+    )
